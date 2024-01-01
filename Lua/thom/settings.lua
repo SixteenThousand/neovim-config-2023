ccd3464a96773ce2c3d51d49ec15c243e7946505
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-01 14:37:43 +0000

    > moved all autocmds of the type "FileType *" into one big autocmd > added some spaces to the spell autocmd from last commit

--- Lua/thom/settings.lua
+++ Lua/thom/settings.lua
@@ -1,10 +1,19 @@
+-- settings that have to be set for every buffer
+vim.api.nvim_create_autocmd("BufEnter",{
+	callback = function(evt)
+		-- line numbers
+		vim.cmd.set("number")
+		vim.cmd.set("relativenumber")
+		vim.cmd.set("noexpandtab")
+		vim.cmd.set("formatoptions-=r formatoptions-=c formatoptions-=o")
+		vim.cmd.set("textwidth=80")
+	end
+})
+
+
 -- default places search will look; note this used to be ".,/usr/include/,,"
 	-- "**2" means "look at any subdirectory that are at most 2 levels down"
 vim.cmd.set("path=.,~/Documents/Programs/**2/,~/Documents/O-Backup_Management/,,")
-
--- line numbers
-vim.cmd.autocmd("FileType * set number")
-vim.cmd.autocmd("FileType * set relativenumber")
 
 -- indentation & autocommenting
 vim.cmd.set("nosmarttab")
@@ -12,12 +21,6 @@ vim.cmd.set("shiftwidth=4")
 vim.cmd.set("tabstop=4")
 vim.cmd.set("cpoptions+=I")
 	-- means autoindenting doesn't get deleted all the time
-vim.cmd.autocmd("FileType * set noexpandtab")
-vim.cmd.autocmd("FileType * set formatoptions-=r formatoptions-=c formatoptions-=o")
-
--- text width
-vim.cmd.set("textwidth=80")
-vim.cmd.autocmd("BufEnter set textwidth=80")
 
 
 -- buffers?? save?? saving?? idk what to call this bit
@@ -36,8 +39,8 @@ vim.cmd.autocmd("FileType lisp set list")
 
 -- spell check
 vim.api.nvim_create_autocmd("BufEnter",{
-	pattern="*.md",
-	callback= function (evt)
+	pattern = "*.md",
+	callback = function (evt)
 		vim.cmd.setlocal("spell")
 	end
 })
